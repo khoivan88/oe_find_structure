33209ca9be1ceb4d9b33b908b4fee6ef157df09e
Khoi Van <khoivan@Khois-MacBook.local>
2018-07-26 13:18:58 -0400

    finding all files missing smiles limit to 50

--- update_sql_mol_v1/update_sql_mol.py
+++ update_sql_mol_v1/update_sql_mol.py
@@ -4,7 +4,7 @@ import mysql.connector as mariadb
 def main():
     mariadb_connection = mariadb.connect(user='root', password='Buchwa1dr0ck5', database='test1')
     cursor = mariadb_connection.cursor()
-    cursor.execute("SELECT cas_nr, molecule_id, smiles, molfile_blob FROM molecule limit 50")
+    cursor.execute("SELECT cas_nr, molecule_id, smiles, molfile_blob FROM molecule where smiles='' limit 50")
 
     for row in cursor:
         print('CAS# {}: {}'.format(row[0], row))
