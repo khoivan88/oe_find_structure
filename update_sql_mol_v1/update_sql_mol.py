380747fddc03fa73d9c315c1c25ca2286d8d2c46
Khoi Van <khoivan@Khois-MacBook.local>
2018-07-26 13:38:00 -0400

    refractor and adding limit query option

--- update_sql_mol_v1/update_sql_mol.py
+++ update_sql_mol_v1/update_sql_mol.py
@@ -2,9 +2,20 @@
 import mysql.connector as mariadb
 
 def main():
-    mariadb_connection = mariadb.connect(user='root', password='Buchwa1dr0ck5', database='test1')
+
+    mariadb_connection, cursor = connect('root', 'Buchwa1dr0ck5', 'test1')
+    search_empty_smile(cursor, 'limit 10')
+    mariadb_connection.close()
+
+def connect(username, pw, database):
+    # Create a cursor:
+    # https://mariadb.com/resources/blog/how-connect-python-programs-mariadb
+    mariadb_connection = mariadb.connect(user=username, password=pw, database=database)
     cursor = mariadb_connection.cursor()
-    cursor.execute("SELECT cas_nr, molecule_id, smiles, molfile_blob FROM molecule where smiles='' limit 50")
+    return mariadb_connection, cursor
+
+def search_empty_smile(cursor, limit=''):
+    cursor.execute("SELECT cas_nr, molecule_id, smiles, molfile_blob FROM molecule where smiles='' {}".format(limit))
 
     for row in cursor:
         print('CAS# {}: {}'.format(row[0], row))
